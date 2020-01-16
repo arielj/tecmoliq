4b46464b7814118921950d27c8ff58aa138bb495
Ariel Juodziukynas <arieljuod@gmail.com>
2020-01-16 20:44:28 -0300

    fix menu/burger en mobile

--- bottom.php
+++
@@ -1,9 +0,0 @@
-  </div>
-  <div class='clear'></div>
-  <div id="footer">
-    <div class='separator'><img src='/img/separador.jpg' alt='' /></div>
-    <div id='webmaster'>Webmaster: <a href="http://www.arieljuod.com.ar" target='_blank'>Ariel Juodziukynas</a></div>
-    <div id='copyright'>&copy TecMoLiq</div>
-    <div class='clear'></div>
-  </div>
-</div>
--- scripts.js
+++ scripts.js
@@ -1,10 +1,42 @@
 document.addEventListener('DOMContentLoaded', function() {
   if (location.search.match(/lang=en/))
     document.body.classList.add('english');
+  
+  setAge();
 
+  bindBurger();
   bindPumps();
   initMap();
 })
+
+function bindBurger() {
+  const burger = document.getElementById('burger');
+  const navBar = document.getElementById('navbar');
+  burger.addEventListener('click', _e => {
+    navBar.classList.toggle('expanded');
+  });
+
+  document.addEventListener('click', e => {
+    if (e.target.id == 'burger') return;
+
+    navBar.classList.remove('expanded');
+  })
+}
+
+function setAge() {
+  dt1 = new Date(1977, 12, 19);
+  dt2 = new Date();
+
+  let diff = (dt2.getTime() - dt1.getTime()) / 1000;
+  
+  diff /= (60 * 60 * 24);
+  
+  years = Math.abs(Math.round(diff/365.25));
+  
+  document.querySelectorAll('.age').forEach( el => {
+    el.innerText = years;
+  })
+}
 
 function initMap() {
   this.mymap = L.map('map').setView([-34.71714803840951, -58.30205535624998], 14);
